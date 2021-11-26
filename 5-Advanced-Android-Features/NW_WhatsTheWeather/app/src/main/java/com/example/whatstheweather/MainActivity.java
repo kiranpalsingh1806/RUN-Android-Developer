5a9f2fda8dc3cca3f826888652f6f355ad20a15b
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-11-26 12:54:26 +0530

    Complete Whats The Weather App

--- 5-Advanced-Android-Features/NW_WhatsTheWeather/app/src/main/java/com/example/whatstheweather/MainActivity.java
+++ 5-Advanced-Android-Features/NW_WhatsTheWeather/app/src/main/java/com/example/whatstheweather/MainActivity.java
@@ -2,13 +2,111 @@ package com.example.whatstheweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        resultTextView = findViewById(R.id.resultTextView);
+    }
+
+    public void getWeather(View view) {
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://api.openweathermap.org/data/2.5/weather?q=" + editText.getText().toString() +  "&appid=b8b67243a7db4c637263167f850d2926");
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpsURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpsURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while(data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch(Exception e) {
+                e.printStackTrace();
+
+                Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT);
+
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherInfo = jsonObject.getString("weather");
+                Log.i("Weather Content", weatherInfo);
+                JSONArray arr = new JSONArray(weatherInfo);
+                String message = "";
+
+                for(int i = 0; i < arr.length(); i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = jsonPart.getString("main");
+                    String description = jsonPart.getString("description");
+
+                    if(!main.equals("") && !description.equals("")) {
+                        message += main + " : " + description + "\r\n";
+                    }
+                }
+
+                if(!message.equals("")) {
+                    resultTextView.setText(message);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT);
+                }
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT);
+                e.printStackTrace();
+            }
+        }
     }
 }
