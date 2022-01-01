756c3a47115fc2f4a73cd4168962f602e9a072b3
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-01 07:51:27 +0530

    Add SQLite Database code

---
+++ 7-Permanent-Data-Storage/DatabaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.databasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3)) ");
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Nick', 28)");
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Kiranpal', 20)");
+
+        Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+        int nameIndex = c.getColumnIndex("name");
+        int ageIndex = c.getColumnIndex("age");
+
+        c.moveToFirst();
+
+        while(!c.isAfterLast()) {
+            Log.i("name", c.getString(nameIndex));
+            Log.i("age", c.getString(ageIndex));
+            c.moveToNext();
+        }
+    }
+}
